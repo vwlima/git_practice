e4507e91fe5996516fdc36b1bfa0d0bc96715db3
vwlima <147662967+vwlima@users.noreply.github.com>
2024-01-24 10:54:59 -0300

    Js arq.v01

--- exercise1_codecademy.js
+++ exercise1_codecademy.js
@@ -1,10 +1,10 @@
 // As frases serão - Show me the money, May the force be with you e You talkin' to me? 
 
-let phrases = ['Show me they money!', 'May the force be with you!', 'You talkin to me?']
+let phrases = ['Show me they money!', 'May the force be with you!', 'You talkin to me?', 'Hello World!']
 
 const printPhrases = function(arr) {
     
-    console.log(arr[Math.floor(Math.random() * 3)]);
+    console.log(arr[Math.floor(Math.random() * 4)]);
     
     }
 
